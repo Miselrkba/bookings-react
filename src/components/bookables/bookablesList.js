834a0c9d72f6a66b6f41114e2287b0efc51bb8a9
iii <miselrkba@gmail.com>
2021-05-11 07:08:26 +0200

    feat: added dropdown to select rooms and kit

--- src/components/bookables/bookablesList.js
+++ src/components/bookables/bookablesList.js
@@ -1,23 +1,45 @@
 import React, { useState } from 'react';
 
 import { bookables } from '../../static.json';
+import { FaArrowRight } from 'react-icons/fa';
 
-const List = () => {
-  const group = 'Rooms';
+const BookablesList = () => {
+  const [group, setGroup] = useState('Kit');
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+  const [bookableIndex, setBookableIndex] = useState(0);
+  const groups = [...new Set(bookables.map((b) => b.group))];
+
+  const nextBookable = () => {
+    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  };
 
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li key={b.id} className={i === bookableIndex ? 'selected' : null}>
-          <button className="btn" onClick={() => setBookableIndex(i)}>
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <select value={group} onChange={(e) => setGroup(e.target.value)}>
+        {groups.map((g) => (
+          <option value={g} key={g}>
+            {g}
+          </option>
+        ))}
+      </select>
+
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? 'selected' : null}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 };
 
-export default List;
+export default BookablesList;
