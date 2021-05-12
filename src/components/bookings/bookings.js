718d9cb6919c555e3adf27188128db0124181212
iii <miselrkba@gmail.com>
2021-05-12 06:22:49 +0200

    feat: added bookings date

--- src/components/bookings/bookings.js
+++ src/components/bookings/bookings.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import WeekPicker from './weekPicker';
 
 const Bookings = () => {
   return (
     <main className="bookings-page">
       <p>Bookings!</p>
+      <WeekPicker date={new Date()} />
     </main>
   );
 };
