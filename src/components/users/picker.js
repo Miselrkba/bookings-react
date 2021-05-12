45d01153ec74a1eee91c6f3b27f094c5d6ea2287
iii <miselrkba@gmail.com>
2021-05-12 08:44:55 +0200

    feat: added json server data fetching and spinner

--- src/components/users/picker.js
+++ src/components/users/picker.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { users } from '../../static.json';
+import Spinner from '../ui/spinner';
 
 const Picker = () => {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+      .then((resp) => resp.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  if (users === null) {
+    return <Spinner />;
+  }
+
   return (
     <select>
       {users.map((u) => (
