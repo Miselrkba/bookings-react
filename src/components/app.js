51a4839ac709a514792d6fce081293e9f54f9c80
iii <miselrkba@gmail.com>
2021-05-14 09:24:42 +0200

    feat: added context for selecting user

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import './app.css';
@@ -9,41 +9,45 @@ import Picker from './users/picker';
 import BookingsPage from './bookings/bookingsPage';
 import Bookables from './bookables/bookables';
 import User from './users/user';
+import UsertContext from './users/userContext';
 
 const App = () => {
+  const [user, setUser] = useState();
   return (
-    <Router>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/bookings" className="btn btn-header">
-                <FaCalendarAlt />
-                <span>Bookings</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/bookables" className="btn btn-header">
-                <FaDoorOpen />
-                <span>Bookables</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/users" className="btn btn-header">
-                <FaUsers />
-                <span>Users</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Picker />
-      </header>
-      <Routes>
-        <Route path="/bookings" element={<BookingsPage />}></Route>
-        <Route path="/bookables" element={<Bookables />}></Route>
-        <Route path="/users" element={<User />}></Route>
-      </Routes>
-    </Router>
+    <UsertContext.Provider value={user}>
+      <Router>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bookings" className="btn btn-header">
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/bookables" className="btn btn-header">
+                  <FaDoorOpen />
+                  <span>Bookables</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/users" className="btn btn-header">
+                  <FaUsers />
+                  <span>Users</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Picker user={user} setUser={setUser} />
+        </header>
+        <Routes>
+          <Route path="/bookings" element={<BookingsPage />}></Route>
+          <Route path="/bookables" element={<Bookables />}></Route>
+          <Route path="/users" element={<User />}></Route>
+        </Routes>
+      </Router>
+    </UsertContext.Provider>
   );
 };
 
--- src/components/users/picker.js
+++ src/components/users/picker.js
@@ -2,23 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 import Spinner from '../ui/spinner';
 
-const Picker = () => {
+const Picker = ({ user, setUser }) => {
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3001/users')
       .then((resp) => resp.json())
-      .then((data) => setUsers(data));
-  }, []);
+      .then((data) => {
+        setUsers(data);
+        setUser(data[0]);
+      });
+  }, [setUser]);
+
+  const handleSelect = (e) => {
+    const selectedID = parseInt(e.target.value, 10);
+    const selectedUser = users.find((u) => u.id === selectedID);
+
+    setUser(selectedUser);
+  };
 
   if (users === null) {
     return <Spinner />;
   }
 
   return (
-    <select>
+    <select className="user-picker" onChange={handleSelect} value={user?.id}>
       {users.map((u) => (
-        <option key={u.id}>{u.name}</option>
+        <option key={u.id} value={u.id}>
+          {u.name}
+        </option>
       ))}
     </select>
   );
