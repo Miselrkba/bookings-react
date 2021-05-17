555d108a2f0b92ee7a6681a2bd46c39ddbb99766
iii <miselrkba@gmail.com>
2021-05-17 09:35:29 +0200

    feat: added suspense & fallback

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
@@ -6,50 +6,57 @@ import './app.css';
 
 import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa';
 
-import Picker from './users/userPicker';
-import BookingsPage from './bookings/bookingsPage';
-import Bookables from './bookables/bookables';
-import User from './users/usersPage';
 import { UserProvider } from './users/userContext';
+import UserPicker from './users/userPicker';
+import PageSpinner from './ui/pageSpinner';
+
+const BookablesPage = lazy(() => import('./bookables/bookablesPage'));
+const BookingsPage = lazy(() => import('./bookings/bookingsPage'));
+const UsersPage = lazy(() => import('./users/usersPage'));
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  const [user, setUser] = useState();
   return (
     <QueryClientProvider client={queryClient}>
       <UserProvider>
         <Router>
-          <header>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/bookings" className="btn btn-header">
-                    <FaCalendarAlt />
-                    <span>Bookings</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/bookables" className="btn btn-header">
-                    <FaDoorOpen />
-                    <span>Bookables</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/users" className="btn btn-header">
-                    <FaUsers />
-                    <span>Users</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <Picker user={user} setUser={setUser} />
-          </header>
-          <Routes>
-            <Route path="/bookings" element={<BookingsPage />}></Route>
-            <Route path="/bookables/*" element={<Bookables />}></Route>
-            <Route path="/users" element={<User />}></Route>
-          </Routes>
+          <div className="App">
+            <header>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/bookings" className="btn btn-header">
+                      <FaCalendarAlt />
+                      <span>Bookings</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/bookables" className="btn btn-header">
+                      <FaDoorOpen />
+                      <span>Bookables</span>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/users" className="btn btn-header">
+                      <FaUsers />
+                      <span>Users</span>
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <UserPicker />
+            </header>
+
+            <Suspense fallback={<PageSpinner />}>
+              <Routes>
+                <Route path="/bookings" element={<BookingsPage />} />
+                <Route path="/bookables/*" element={<BookablesPage />} />
+                <Route path="/users" element={<UsersPage />} />
+              </Routes>
+            </Suspense>
+          </div>
         </Router>
       </UserProvider>
     </QueryClientProvider>
